4c4270bf94051534e5d0cbbf78fb24a15ef0e980
Guilherme Lino <guilhermelinosp@outlook.com>
2024-10-25 12:47:09 -0300

    up

--- ios/ReactNativeLatestTests/ReactNativeLatestTests.m
+++ ios/ReactNativeLatestTests/ReactNativeLatestTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface MyReactNativeAppTests : XCTestCase
+@interface ReactNativeLatestTests : XCTestCase
 
 @end
 
-@implementation MyReactNativeAppTests
+@implementation ReactNativeLatestTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
